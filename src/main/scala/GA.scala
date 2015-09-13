c708e96fd8159cc195a500f4598d5545a14e163c
Brett Menzies <scimor5@gmail.com>
2015-09-13 13:18:24 -0700

    Initial GA implementation

---
+++ src/main/scala/GA.scala
@@ -0,0 +1,65 @@
+package com
+
+import scala.collection.generic.CanBuildFrom
+import scala.util.Random
+import scala.collection.mutable.ArrayBuffer
+
+class GA (popSize: Int, genMax: Int, tSize: Int) extends Optimizer {
+    var averages : Array[Double] = null;
+    var bests    : Array[Double] = null;
+
+    def apply[T <: Solution[T]] (Factory: ()=>T): (Seq[Double], T) = {
+        averages = Array.ofDim[Double](genMax)
+        bests    = Array.ofDim[Double](genMax)
+
+        var pop = for(x <- 1 to popSize) yield Factory()
+
+        for(i <- 0 until genMax){
+            pop = (1 to popSize by 2).flatMap { x=>
+                val parentA : T = tournament(pop, tSize)(MinOrd)
+                val parentB : T = tournament(pop, tSize)(MinOrd)
+                val (childA, childB) = parentA.crossover(parentB)
+                List(childA.mutate(), childB.mutate())
+            }
+
+            //log average population fitness
+            averages(i) = pop.foldLeft(0.0)(_ + _.fitness) / pop.size.toDouble
+            bests(i)    = pop.max(MinOrd).fitness
+        }
+
+        (averages, pop.max(MinOrd))
+    }
+
+    /**
+     * Takes a random sampling of `num` elements from `xs` with replacement
+     * Returns a new collection of the same type as `xs` whenever possible
+     */
+    def randomTake[T, CC[X] <: TraversableOnce[X]](xs: CC[T], num: Int)
+      (implicit bf: CanBuildFrom[CC[T], T, CC[T]]): CC[T] = {
+        //make array buffer for random access shuffling
+        val buf = new ArrayBuffer[T] ++= xs
+        val nb  = bf(xs)
+
+        for(x <- 1 to num)
+            nb += buf(GA.rand.nextInt(buf.size))
+
+        nb.result()
+    }
+
+    class MinimizeOrd extends Ordering[Solution[_]] {
+        def compare(a: Solution[_], b: Solution[_]) =
+            b.fitness compare a.fitness
+    }
+    implicit object MinOrd extends MinimizeOrd
+
+    def tournament[T <: Solution[T]](xs: TraversableOnce[T], num: Int)
+      (implicit ord: Ordering[Solution[_]]): T = {
+        val bracket = randomTake(xs, num)
+        bracket.max(ord)
+    }
+}
+
+object GA {
+    private val rand = new Random()
+
+}
--- src/main/scala/launch.scala
+++ src/main/scala/launch.scala
@@ -5,7 +5,7 @@ import swing._
 
 object App extends SimpleSwingApplication{
 
-    val runs = (for(x <- (1 to 1).par) yield {
+/*    val runs = (for(x <- (1 to 1).par) yield {
         val optimizer = new Annealing(1000000, 200)
         val (run, best) = optimizer(()=>Schwefel.randomSolution(30, 5.0f))
 
@@ -16,7 +16,29 @@ object App extends SimpleSwingApplication{
                 run(index).toDouble
             }
         }
-    }).seq
+    }).seq*/
+
+    val optimizer = new GA(50, 250, 3)
+    val (_, best) = optimizer(()=>Sphere.randomSolution(30, 0.06f))
+
+    val runs = List(
+        new DataSource(){
+            val getName = "Average"
+            def get(X: Double): Double = {
+                val index :Int = (X*optimizer.averages.size.toDouble).toInt
+                optimizer.averages(index).toDouble
+            }
+        },
+        new DataSource(){
+            val getName = "Best"
+            def get(X: Double): Double = {
+                val index :Int = (X*optimizer.bests.size.toDouble).toInt
+                optimizer.bests(index).toDouble
+            }
+        }
+    )
+
+    println(runs.size)
 
     def top = new MainFrame{
         title = "Optimizer"
