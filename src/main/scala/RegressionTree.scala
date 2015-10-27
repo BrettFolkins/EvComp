f79f80dc28706e998f1d20437eb0acb7e970a360
Brett Menzies <scimor5@gmail.com>
2015-10-26 20:42:02 -0700

    A bunch of expirimentation with GA's and Regression Trees

--- src/main/scala/RegressionTree.scala
+++ src/main/scala/RegressionTree.scala
@@ -5,13 +5,13 @@ import com.expTree._
 import scala.util.Random
 
 object RegressionTree {
-    def apply(ds: DataSet, iHeight: Int): Problem = {
+    def apply(ds: DataSet, iHeight: Int, parsimony: Double): Problem = {
 
-        val algebra = new Algebra {
-            def randomVarIndex() = rand.nextInt(ds.vectorLen)
+        val nodeSet = new Algebra {
             def randomConstantValue(): Double = ((rand.nextDouble()-0.5)*2.0)*ds.range
+            def randomVarIndex() = rand.nextInt(ds.vectorLen)
             case class Variable(i: Int) extends ExpNode {
-                def this() = this(0)
+                def this() = this(randomVarIndex())
                 val children = Nil
                 def buildUsing(subs: Iterator[ExpNode]) = new Variable(randomVarIndex())
                 def eval(v: Any) = {
@@ -26,33 +26,62 @@ object RegressionTree {
                                                new Add(),      new Subtract(),
                                                new Multiply(), new Divide()   )
         }
-        import algebra._
 
         class Tree(val t: ExpNode) extends Solution[Tree] {
             def inspect = t
             val fitness = {
-                (for((data,target) <- ds.data) yield {
+                val ms = (for((data,target) <- ds.data) yield {
                     val ans  = t.eval(data)
                     val diff = target - ans
                     diff*diff
-                }).sum + t.size
+                }).sum
+                val rms = Math.sqrt(ms)
+
+                //rms
+                rms + parsimony * t.size
+                //(rms+1.0) * (1.0 + parsimony * t.size)
             }
             def mutate(): Tree = {
-                new Tree(algebra.mutateRandomNode(t))
+                //new Tree(nodeSet.mutateRandomNode(t))
+
+                val r = nodeSet.rand.nextDouble;
+/*                if(r <= 0.05) {
+                    //replace random subtree
+                    val toReplace  = nodeSet.rand.nextInt(t.size)
+                    //val oldSubtree = t.pickSubtree(toReplace)
+                    //val newDepth   = oldSubtree.depth / 2
+                    val newSubtree = (new nodeSet.TreeGenerator(iHeight)).next()
+                    new Tree(t.replaceSubtree(toReplace,newSubtree))
+                } else */
+                if (r <= 0.10) {
+                    //become random subtree
+                    new Tree(t.pickSubtree(nodeSet.rand.nextInt(t.size)))
+                } else if (r <= 0.30) {
+                    //mutate random node
+                    new Tree(nodeSet.mutateRandomNode(t))
+                } else {
+                    //no mutation
+                    this
+                }
             }
             def crossover(other: Tree): (Tree, Tree) = {
                 val l = t
                 val r = other.t
-                val ( left, lidx) = algebra.getBaisedSubtree(l, 0.9)
-                val (right, ridx) = algebra.getBaisedSubtree(r, 0.9)
+                val ( left, lidx) = nodeSet.getBiasedSubtree(l, 0.8)
+                val (right, ridx) = nodeSet.getBiasedSubtree(r, 0.8)
                 (new Tree(l.replaceSubtree(lidx, right)),
                  new Tree(r.replaceSubtree(ridx, left) ) )
+
+/*
+                val (l,r) = nodeSet.crossoverSubtrees(t, other.t)
+                (new Tree(l), new Tree(r))*/
+
             }
             override def toString() = t.toString + " size: " + t.size
         }
 
         new Problem {
-            val generator = new algebra.TreeGenerator(iHeight)
+            val generator = new nodeSet.TreeGenerator(iHeight)
             type SolutionType = Tree
             def potential() = new Tree(generator.next());
         }
--- src/main/scala/launch.scala
+++ src/main/scala/launch.scala
@@ -5,33 +5,49 @@ import com.expTree._
 import com.graph._
 import scala.util.Random
 import swing._
-import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.ArrayBuffer
 
-// load DS from file
-// elietism
 // cooler mutation
-// way to graph final function
-// better parsimony pressure
-// graph size, fitness of population
+// way to graph final function y vals
 // better regression tree paramaterization
 // clean up algebra and tree based solution creation?
 // write a tree simplify function
 
 object App {
     def main(args: Array[String]) {
-        val testDS  = DataSet.fromFunc(3, 50, 5.0){ x => x.map(y => y*y*y).sum }
-        val problem = RegressionTree(testDS, 3)
-        val solver  = new GA(popSize = 200, genMax = 200, tournamentSize=3)
-        val runData = new ListBuffer[Double]()
+        //val testDS  = DataSet.fromFunc(1, 50, 3.14){ x => Math.exp(x(0)) }
+        val testDS  = DataSet.fromFunc(3, 50, 10.0){ x => x.map(y => y*y*y).sum }
+        //val testDS = DataSet.fromFile("GPProjectData.csv")
+
+        val problem = RegressionTree(testDS, 6, 1.50)
+        val solver  = new GA(popSize = 1000, genMax=1000, tournamentSize=8)
+        //val solver  = new HillClimb(1000)
+
+        val best    = new ArrayBuffer[Double]()
+        val average = new ArrayBuffer[Double]()
+        val size    = new ArrayBuffer[Double]()
+        val Diag = new Diagnostic[problem.SolutionType]{
+            def log(pop: Seq[problem.SolutionType]) {
+                val fits = pop.map(x => x.fitness)
+                best    += fits.min
+                average += fits.sum / pop.size.toDouble
+                size    += pop.map(x => x.inspect.asInstanceOf[ExpNode].size).sum / pop.size.toDouble
+            }
+        }
 
         new Thread {
             override def run() = {
-                val ans = solver(problem)(Diagnostic.best(runData))
+                val ans = solver(problem)(Diag)
                 println(ans)
+                val tree  = ans.inspect.asInstanceOf[ExpNode]
+                val stree = BasicAlgebra.simplify(tree)
+                println(stree)
             }
         }.start
 
-        val data = Seq(new ArrayDataSource("Best", runData))
+        val data = Seq(new ArrayDataSource("Best", best),
+                       new ArrayDataSource("Average", average),
+                       new ArrayDataSource("Size", size))
         (new GraphWindow(data)).startup(Array())
     }
 }
