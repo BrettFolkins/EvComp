0c17c8270ecfba8d20d3366623be30cc8fd7cf65
Brett Menzies <scimor5@gmail.com>
2017-07-11 09:08:45 -0700

    One not very deterministic benchmark

--- src/jmh/scala/JmhScala.scala
+++ src/jmh/scala/JmhScala.scala
@@ -3,7 +3,23 @@ package com
 import org.openjdk.jmh._
 import org.openjdk.jmh.annotations._
 
+import com.ml._
+import com.ml.CGP._
+
+@State(Scope.Benchmark)
 class JmhScala {
-    @Benchmark def helloFromScala() {
+    val CGP = new CGP(
+        new FitnessEval {
+            val range = 1.0
+            val inputCount = 10
+            val outputCount = 10
+            def apply(func: Seq[Double] => Seq[Double]) =
+                func((1 to 10).map{_.toDouble}).sum
+        },
+        Node.algebra(1.0),
+        2048)
+
+    @Benchmark def randomCgpEvalute() {
+        CGP.potential().fitness
     }
 }
